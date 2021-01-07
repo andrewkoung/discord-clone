b125c070a5d7fcf6da37027da595303320ad0a75
Andrew <andrewpkoung@gmail.com>
2021-01-07 17:50:24 -0500

    setting up css grid

--- client/src/components/Pages/Dashboard/index.js
+++ client/src/components/Pages/Dashboard/index.js
@@ -1,10 +1,11 @@
 import { withFirebase } from './../../Firebase';
+import './style.css'
 
 function Dashboard({ firebase }) {
     return (
         <>
             <div className="dashboard">
-
+                <p>hi</p>
             </div>
         </>
     )
