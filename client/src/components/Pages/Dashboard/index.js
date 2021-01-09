d97b8b99de98827fd864f1724acc8022ccd67852
Andrew <andrewpkoung@gmail.com>
2021-01-08 22:36:59 -0500

    small changes

--- client/src/components/Pages/Dashboard/index.js
+++ client/src/components/Pages/Dashboard/index.js
@@ -1,11 +1,27 @@
 import { withFirebase } from './../../Firebase';
 import './style.css'
+import { useAuth } from './../../Session/AuthContext';
+import { useEffect } from 'react';
 
 function Dashboard({ firebase }) {
+    const { currentUser } = useAuth(); 
+
+    useEffect(() => {
+        console.log(currentUser)
+    })
+
     return (
         <>
             <div className="dashboard">
-                <p>hi</p>
+                <div>
+                    <p>Hi</p>
+                </div>
+                <div>
+                    <p>Hi</p>
+                </div>
+                <div>
+                    <p>Hi</p>
+                </div>
             </div>
         </>
     )
