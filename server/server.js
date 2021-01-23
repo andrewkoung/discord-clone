bfa7230a2cd8fba7ca5e0e5408c2aae1592b0cee
Andrew <andrewpkoung@gmail.com>
2021-01-22 23:41:43 -0500

    testing server code

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema')
+
+const app = express(); 
+
+app.use(cors())
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+);
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+© 2021 GitHub, Inc.
